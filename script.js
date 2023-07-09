69a116464bb171d115d19ff1104f74c1bad22d89
AdarshMitr <adarshkumar019@gmail.com>
2023-07-09 20:02:35 +0530

    added styles

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-import users from "./users";
+import users from "./users.js";
+
 
 // 1. Write a program to get an array of names from a given array of users.
 
 //2. Get back only active users.
 
 //3. Sort users by age in descending order.
+
+
+let Names=[];
+
+for(let i=0;i<users.length;i++){
+    Names.push(users[i].name);
+    console.log(Names)
+}
+
+let box=document.getElementById('box');
+box.innerHTML=Names;
--- users.js
+++ users.js
@@ -63,4 +63,4 @@ const users=[
 ];
 
 
-export  default users;
+export default users;
