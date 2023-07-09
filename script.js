3f12b63e99d8074daae60c1c2b44fe0a4e34510b
AdarshMitr <adarshkumar019@gmail.com>
2023-07-09 15:12:23 +0530

    users file added

---
+++ script.js
@@ -0,0 +1,7 @@
+import users from "./users";
+
+// 1. Write a program to get an array of names from a given array of users.
+
+//2. Get back only active users.
+
+//3. Sort users by age in descending order.
---
+++ users.js
@@ -0,0 +1,66 @@
+const users=[
+    {
+        id:1,
+        name:'Hari',
+        isActive:true,
+        age:22,
+    },
+    {
+        id:2,
+        name:'Nikhli',
+        isActive:false,
+        age:22,
+    },
+    {
+        id:3,
+        name:'Sundar',
+        isActive:true,
+        age:30,
+    },
+    {
+        id:4,
+        name:'Damodar',
+        isActive:false,
+        age:33,
+    },
+    {
+        id:5,
+        name:'Chandan',
+        isActive:true,
+        age:30,
+    },
+    {
+        id:6,
+        name:'Bhushan',
+        isActive:false,
+        age:35,
+    },
+    {
+        id:7,
+        name:'Gagan',
+        isActive:true,
+        age:21,
+    },
+    {
+        id:8,
+        name:'Amit',
+        isActive:false,
+        age:29,
+    },
+    {
+        id:9,
+        name:'Sumit',
+        isActive:false,
+        age:25,
+    },
+    {
+        id:10,
+        name:'Satyajeet',
+        isActive:true,
+        age:38,
+    },
+
+];
+
+
+export  default users;
