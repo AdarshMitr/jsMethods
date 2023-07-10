b0c7d1b0f5421157a7f259904da83c35b971fff0
AdarshMitr <adarshkumar019@gmail.com>
2023-07-10 20:36:15 +0530

    forEach, Map,filter and Sort functions

--- script.js
+++ script.js
@@ -11,15 +11,87 @@ import users from "./users.js";
 
 
 // -----------------Solution 1-----------------------
-// using for loop:
+// using for loop (level 1):
 
 
 let Names=[];
 
+/*
 for(let i=0;i<users.length;i++){
     Names.push(users[i].name);
-    console.log(Names)
+    //console.log(Names)
 }
+*/
+
+
+
+// using forEach method
+//* forEach is an iterative method that does not return anything.It always return undefined and is unchainable. It does not mutates the array. It expects a synchronous function -it does not wait for promises.
+
+/*
+users.forEach((user)=>{
+    Names.push(user.name)
+    //console.table(Names);
+})
+
+*/
+
+//   ----------Solution 2------------
+
+//using for loop
+
+/*
+for (let i=0;i<users.length;i++){
+    if(users[i].isActive){
+        Names.push(users[i].name)
+    }
+    console.log(Names);
+}
+
+*/
+//using forEach method
+
+/*
+users.forEach((user)=>{
+    if(user.isActive){
+    Names.push(user.name)
+    }
+})
+*/
+
+
+//using map method:
+//It returns a new array.
+/*
+Names=users.filter((user)=>user.isActive).map((user)=>user.name)
+
+console.log(Names);
+*/
+
+//-------------Solution 3------------
+
+// Sort Function
+
+//in ascending age order
+/*
+users.sort((user1,user2)=>
+    user1.age < user2.age ? -1 :1);
+console.log(users);
+*/
+
+//in descending age order
+/*
+users.sort((user1,user2)=>
+    user1.age < user2.age ? 1 :-1);
+console.log(users);
+
+*/
+
+Names=users.filter((user)=>user.isActive).sort((user1,user2)=>
+user1.age < user2.age ? 1 :-1).map((user)=>user.name)
+
+console.log(Names);
+
 
 let box=document.getElementById('box');
 box.innerHTML=Names;
